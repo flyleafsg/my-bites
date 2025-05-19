7e01329e3ee2dcd63212ab3eac33e6cbc03b685f
Gina Christians <gina.christians@colostate.edu>
2025-05-19 14:41:54 -0700

    Pulled in changes from old version and cleaned up assets

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,29 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'ClassCountdown',
-    slug: 'classcountdown',
+    name: 'My Bites',
+    slug: 'my-bites',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      bundleIdentifier: 'com.yourcompany.mybites'
+    },
+    android: {
+      package: 'com.yourcompany.mybites'
+    },
     extra: {
       EXPO_PUBLIC_FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
       EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
       EXPO_PUBLIC_FIREBASE_PROJECT_ID: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
       EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
       EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      EXPO_PUBLIC_FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-    },
-  },
+      EXPO_PUBLIC_FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+    }
+  }
 };
+
