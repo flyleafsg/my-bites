7e5321d94e77ecccc9b5908e1e473c91fe6c32e2
Gina Christians <gina.christians@colostate.edu>
2025-05-27 18:57:25 -0700

    Update Detox init.js to use Jest adapter for full lifecycle

--- e2e/init.js
+++ e2e/init.js
@@ -1,17 +1,28 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
+const adapter = require('detox/runners/jest/adapter');
+
+jest.setTimeout(120000);
+jasmine.getEnv().addReporter(adapter);
 
 beforeAll(async () => {
   await detox.init(config);
+}, 300000);
+
+beforeEach(async () => {
+  await adapter.beforeEach();
+});
+
+afterEach(async () => {
+  await adapter.afterEach();
+
+  // Optional: Always take a screenshot, especially for debugging
+  if (device.takeScreenshot) {
+    const testName = expect.getState().currentTestName.replace(/\s+/g, '_');
+    await device.takeScreenshot(`AFTER_${testName}`);
+  }
 });
 
 afterAll(async () => {
   await detox.cleanup();
-});
-
-afterEach(async () => {
-  if (device.takeScreenshot) {
-    const testName = expect.getState().currentTestName.replace(/\s+/g, '_');
-    await device.takeScreenshot(`FAIL_${testName}`);
-  }
 });
