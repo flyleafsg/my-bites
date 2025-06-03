0f77f091405886b7ace9c075f5b2477f35626fb8
Gina Christians <gina.christians@colostate.edu>
2025-06-03 14:54:03 -0700

    added onboarding into stack.navigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from '../screens/HomeScreen';
 import LogMealScreen from '../screens/LogMealScreen';
@@ -29,29 +28,18 @@ const AppNavigator = () => {
   if (isLoading) return null;
 
   return (
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {hasOnboarded ? (
-          <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="LogMeal" component={LogMealScreen} />
-            <Stack.Screen name="MealHistory" component={MealHistoryScreen} />
-            <Stack.Screen name="LogWater" component={LogWaterScreen} />
-            <Stack.Screen name="WaterHistory" component={WaterHistoryScreen} />
-            <Stack.Screen name="Badges" component={BadgeCollectionScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="LogMeal" component={LogMealScreen} />
-            <Stack.Screen name="MealHistory" component={MealHistoryScreen} />
-            <Stack.Screen name="LogWater" component={LogWaterScreen} />
-            <Stack.Screen name="WaterHistory" component={WaterHistoryScreen} />
-            <Stack.Screen name="Badges" component={BadgeCollectionScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    
+    <Stack.Navigator
+      initialRouteName={hasOnboarded ? 'Home' : 'Onboarding'}
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="LogMeal" component={LogMealScreen} />
+      <Stack.Screen name="MealHistory" component={MealHistoryScreen} />
+      <Stack.Screen name="LogWater" component={LogWaterScreen} />
+      <Stack.Screen name="WaterHistory" component={WaterHistoryScreen} />
+      <Stack.Screen name="Badges" component={BadgeCollectionScreen} />
+    </Stack.Navigator>
   );
 };
 
